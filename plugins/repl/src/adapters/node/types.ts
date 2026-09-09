1a7059d80d5c6d60e17637a3bb51c2899fdb1d29
Conrad Emde <65224188+mdc-git@users.noreply.github.com>
2026-09-10 01:40:14 +0200

    refactor: remove redundant startup cleanup flags

--- plugins/repl/src/adapters/node/types.ts
+++ plugins/repl/src/adapters/node/types.ts
@@ -4,7 +4,6 @@ import type { CleanupResult } from '../process-group.ts'
 export class NodeStartupError extends Error {
   constructor(
     message: string,
-    readonly isCleanupConfirmed: boolean,
     readonly retryCleanup?: () => Promise<CleanupResult>
   ) {
     super(message)
